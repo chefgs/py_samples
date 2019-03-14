d6d3e5ff91b8c74b8d10af8c0b46d2ba762f2876
Saravanan G <g.gsaravanan@gmail.com>
2019-03-14 18:41:12 +0530

    Create arithmatic.py

---
+++ arithmatic.py
@@ -0,0 +1,16 @@
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+    sum = a + b
+    print(str(sum))
+
+    if b < a:
+      diff = a - b
+      print(str(diff))
+    else:
+      print("Difference: Input b is greater than a. So output will be in negative, hence ignored")
+    
+    prod = a * b
+    print(str(prod))
+
+
